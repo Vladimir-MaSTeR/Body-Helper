ff51c252dadfc2128b34681f7d73fa8ae00a5524
Цюман Владимир Владимирович <9e4cA5GUD3>
2021-11-23 01:53:19 +0700

    T-5 Работа с сохранением текста из первой активити и передача во вторую активити. Добавление иконок

--- app/src/main/java/com/tsyuman/bodyhelper/MainActivity.java
+++ app/src/main/java/com/tsyuman/bodyhelper/MainActivity.java
@@ -1,13 +1,20 @@
 package com.tsyuman.bodyhelper;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.id_tv_userName_mainActivity)
+    TextView txUserName;
+
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);             // Butter Knife start
 
+        // прем текста от предыдущего активити через Интент с помощью getStringExtra
+//        Intent intent = getIntent(); // Получаем интент со стартового активити в котором есть передаваемый текст
+//        String userName = intent.getStringExtra("userName"); // получаем имя пользователя по ключу.
+
+        loadText(txUserName);
+    }
+
+    private void loadText(TextView textView) {
+        preferences = getSharedPreferences("myPrev", MODE_PRIVATE);
+        String loadUserName = preferences.getString("saved_user_name", "");
+        textView.setText(String.format("%s\nДобро пожаловать", loadUserName));
     }
 }
